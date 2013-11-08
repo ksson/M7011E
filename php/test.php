867d92c30897ad42a15aef6c2e10ee6858ffd991
ksson <micke_ksson@hotmail.com>
2013-11-08 11:39:46 +0100

    Test

---
+++ php/test.php
@@ -0,0 +1,23 @@
+<!-- overlay trigger element -->
+<a id="formDemo" rel="#sheet"></a>
+ 
+<!-- the overlay -->
+<div id="sheet" class="rounded">
+ 
+  <!-- the form is on the first tab pane -->
+  <div class="pane" id="pane1">
+ 
+    <form class="rounded" id="myform">
+ 
+    </form>
+  </div>
+ 
+  <!-- second tab pane -->
+  <div class="pane" id="pane2"> ...  </div>
+ 
+  <!-- other tab panes ... -->
+  <div class="pane" id="pane3"> ...  </div>
+  <div class="pane" id="pane4"> ...  </div>
+  <div class="pane" id="pane5"> ...  </div>
+  ...
+</div>
